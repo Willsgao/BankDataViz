b9a566a8e84790fdaa5b80c6c288b3c9098144ff
wills <willsgao@163.com>
2025-12-13 15:40:03 +0800

    merge

--- backend_run.py
+++ backend_run.py
@@ -1,14 +1,29 @@
 from backend.app import app
 from flask_cors import CORS
 
-# 完整的 CORS 配置
+# # 完整的 CORS 配置
+# CORS(
+#     app,
+#     origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+#     supports_credentials=True,
+#     allow_headers=["*"],
+#     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]  # ✅ 包含 OPTIONS
+# )
+
+
 CORS(
     app,
-    origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://172.17.0.1:8080",      # 容器内网
+        "http://your-domain.com:8080"  # 公网或域名
+    ],
     supports_credentials=True,
     allow_headers=["*"],
-    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]  # ✅ 包含 OPTIONS
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
 )
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
