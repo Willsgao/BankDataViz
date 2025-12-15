15c9c0b511abcd8ea0cfdf98d98718d56a930390
wills <willsgao@163.com>
2025-12-15 17:35:30 +0800

    merge

--- start_server.sh
+++ start_server.sh
@@ -2,7 +2,7 @@
 # 线上环境 IP:10.19.1.14 port:10272
 
 cd /data/BankTables/
-source source backend/venv/bin/activate
+source backend/venv/bin/activate
 nohup python3 backend_run.py  > logs/app.log 2>&1 &
 
 
