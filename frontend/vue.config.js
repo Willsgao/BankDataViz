089cdfba1ee5572bcaa31977a9873f5358593226
wills <willsgao@163.com>
2026-01-09 13:13:23 +0800

    mm

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 // vue.config.js
-const API_BASE = process.env.VUE_APP_API_BASE || 'http://localhost:5000'
+//const API_BASE = process.env.VUE_APP_API_BASE || 'http://localhost:5000'
+const API_BASE = process.env.VUE_APP_API_BASE || 'http://122.51.196.65:5000'
 
 
 module.exports = defineConfig({
