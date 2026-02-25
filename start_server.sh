b308b9a39ce6023496ca1d74b683e685acdac420
wills <willsgao@163.com>
2026-02-25 14:21:31 +0800

    增加redis服务

--- start_server.sh
+++ start_server.sh
@@ -7,6 +7,7 @@ sudo chmod -R 777 data
 
 nohup python3 backend_run.py  > logs/app.log 2>&1 &
 
+nohup python3 backend/workers/table_worker.py  > logs/redis_worker.log 2>&1 &
 
 cd frontend
 nohup npm run serve &
